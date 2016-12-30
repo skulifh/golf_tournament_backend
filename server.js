161961b6f72ee10f5c6a1d6f87cffd24ca2d2c20
Skúli Freyr Hinriksson <skuli@sareye.com>
2016-12-30 22:16:33 +0100

    Now saving a message from the frontend to the mongo server

--- server.js
+++ server.js
@@ -1,9 +1,11 @@
 var express = require('express');
 var app = express();
 var bodyParser = require('body-parser');
-var mongo = require('mongodb').MongoClient;
+var mongoose = require('mongoose');
 
-var database;
+var Message = mongoose.model('Message', {
+    msg: String
+})
 
 app.use(bodyParser.json());
 app.use(function(req,res,next) {
@@ -14,14 +16,22 @@ app.use(function(req,res,next) {
 
 app.post('/api/message', function(req,res) {
     console.log(req.body)
-    database.collection('messages').insertOne(req.body);
+    var message = new Message(req.body);
+    message.save();
     res.status(200);
 })
 
-mongo.connect("mongodb://localhost:27017/test", function(err,db) {
+function GetMessages()
+{
+    Message.find({}).exec(function(err, result) {
+        console.log(result);
+    })
+}
+
+mongoose.connect("mongodb://localhost:27017/test", function(err,db) {
     if(!err){
         console.log("we are connected to mongo");
-        database = db;
+        GetMessages();
     }
 })
 
