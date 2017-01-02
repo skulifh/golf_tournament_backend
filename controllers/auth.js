bc7b4926c40cabf4cb16b7c5a969391c1c9c38e0
Skúli Freyr Hinriksson <skuli@sareye.com>
2017-01-02 22:22:00 +0100

    Implemented the login functionality

--- controllers/auth.js
+++ controllers/auth.js
@@ -3,25 +3,52 @@ var jwt = require('jwt-simple');
 var moment = require('moment');
 
 module.exports = {
-    register: function(req, res) {
+    register: function (req, res) {
         console.log(req.body);
+
         User.findOne({
             email: req.body.email
-        }, function(err, existingUser){
+        }, function (err, existingUser) {
 
-            if(existingUser)
-                return res.status(409).send({message: 'Email is already registered'});
+            if (existingUser)
+                return res.status(409).send({
+                    message: 'Email is already registered'
+                });
 
             var user = new User(req.body);
 
-            user.save(function(err,result){
+            user.save(function (err, result) {
                 if (err) {
                     res.status(500).send({
                         message: err.message
-                    })
+                    });
                 }
-                res.status(200).send({token: createToken(result)});
+                res.status(200).send({
+                    token: createToken(result)
+                });
             })
+        });
+    },
+    login: function (req, res) {
+        User.findOne({
+            email: req.body.email
+        }, function (err, user) {
+
+            if (!user)
+                return res.status(401).send({
+                    message: 'Email or Password invalid'
+                });
+
+            if (req.body.pwd == user.pwd) {
+                console.log(req.body, user.pwd)
+                res.send({
+                    token: createToken(user)
+                });
+            } else {
+                return res.status(401).send({
+                    message: 'Invalid email and/or password'
+                });
+            }
         });
     }
 }
--- server.js
+++ server.js
@@ -20,6 +20,8 @@ app.post('/api/message',checkAuthenticated, message.post);
 
 app.post('/auth/register', auth.register);
 
+app.post('/auth/login', auth.login);
+
 //Connection
 mongoose.connect("mongodb://localhost:27017/test", function (err, db) {
     if (!err) {
